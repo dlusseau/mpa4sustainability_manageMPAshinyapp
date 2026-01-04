44b479fd921ce1ee8257765497fdd2b3d127d1af
David Lusseau <davlu@dtu.dk>
2026-01-04 14:33:15 +0100

    app built 0

---
+++ app.R
@@ -0,0 +1,6 @@
+##############################################
+#### app to explore how changing human activities occurring in MPAs can affect biodiversity risk
+#### this app was created in the Biodiversa project mpa4sustainability
+#### it relies on data collated over the 2021-2023 period
+#### please ensure that you read the details of the data used before interpreting your results
+#### for any queries, comments or interests email David Lusseau (davlu@dtu.dk)
